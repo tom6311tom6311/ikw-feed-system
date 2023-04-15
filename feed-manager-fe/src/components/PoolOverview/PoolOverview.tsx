ddd16a225d3fc6ea00c5096240fa441c80717d46
Tom Huang <tom6311tom6311@gmail.com>
2023-04-15 16:13:09 +0800

    camera ui in pool overview
    Signed-off-by: Tom Huang <tom6311tom6311@gmail.com>

--- feed-manager-fe/src/components/PoolOverview/PoolOverview.tsx
+++ feed-manager-fe/src/components/PoolOverview/PoolOverview.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { PoolStatus } from '../../types';
+import cameraSample from '../../img/camera_sample.png';
 
 type PoolOverviewProps = {
   poolName: string;
@@ -29,6 +30,9 @@ export default function PoolOverview(props: PoolOverviewProps) {
         <div className="o-siteoverview-info">
           <p className="c-siteoverview-info__key">狀態</p>
           <p className="c-siteoverview-info__value">{status}</p>
+        </div>
+        <div className="o-sitecamera__view">
+          <img src={cameraSample} alt="realtime camera view" className="c-sitecamera-img" />
         </div>
       </div>
     </div>
