605ffee5dcbb4d55db5b5e5784ab5bc4ecc562f5
Tom Huang <tom6311tom6311@gmail.com>
2023-04-15 18:06:25 +0800

    feat: refresh pool overview snapshots periodically
    Signed-off-by: Tom Huang <tom6311tom6311@gmail.com>

--- feed-manager-fe/src/containers/SitePage/SitePage.tsx
+++ feed-manager-fe/src/containers/SitePage/SitePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import AppConfig from '../../const/AppConfig';
 import { useGetSiteQuery } from './GetSiteQuery.graphql.generated';
@@ -10,6 +10,19 @@ function SiteStatusPage() {
     data: getSiteData,
   } = useGetSiteQuery({ variables: { siteId: siteId || '' } });
 
+  const [salt, setSalt] = useState(0);
+  let refreshInterval: number = -1;
+  useEffect(() => {
+    refreshInterval = window.setInterval(() => {
+      setSalt(salt + 1);
+    }, AppConfig.TIMING.IMG_REFRESH_PERIOD);
+
+    return () => {
+      window.clearInterval(refreshInterval);
+      refreshInterval = -1;
+    };
+  }, [salt]);
+
   return (
     <div className="o-sitelist-container">
       {getSiteData?.site?.pools?.map(({ poolId, poolName, status }) => (
@@ -18,7 +31,7 @@ function SiteStatusPage() {
           poolName={poolName}
           status={status}
           link={`/site/${siteId}/pool/${poolId}`}
-          imageUrl={`${AppConfig.BACKEND.URL}/snapshot/${siteId}/${poolId}`}
+          imageUrl={`${AppConfig.BACKEND.URL}/snapshot/${siteId}/${poolId}?salt=${salt}`}
         />
       ))}
     </div>
